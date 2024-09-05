32ba5b46e7318e25970babb71850798f8adb3d68
GourySanal <gourysanal12345@gmail.com>
2024-09-05 22:55:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function greet() {
+    alert("Hello, welcome to my page!");
+}
+
+
+document.getElementById('profile-pic').addEventListener('mouseout', function() {
+    document.body.style.backgroundColor = '#f0f8ff'; // Back to the original color
+});
+
+
